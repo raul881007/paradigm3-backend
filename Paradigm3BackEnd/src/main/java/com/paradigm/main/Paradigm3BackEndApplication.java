9d07fded63a29c142c91066fc681a4603486eb4a
equipo <raul881007@gmail.com>
2021-03-15 16:36:39 -0400

    Initial commit

---
+++ Paradigm3BackEnd/src/main/java/com/paradigm/main/Paradigm3BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.paradigm.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paradigm3BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paradigm3BackEndApplication.class, args);
+	}
+
+}
---
+++ Paradigm3BackEnd/src/test/java/com/paradigm/main/Paradigm3BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigm.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paradigm3BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
